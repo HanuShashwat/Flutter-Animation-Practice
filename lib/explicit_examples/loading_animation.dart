5fa893af9c7003ac442e722689e99ce6b8599ee5
Hanu Shashwat <hanushashwat733@gmail.com>
2026-02-10 21:44:09 +0530

    Build the animation logic.

--- lib/explicit_examples/loading_animation.dart
+++ lib/explicit_examples/loading_animation.dart
@@ -36,31 +36,38 @@ class _RadialProgressAnimationState extends State<RadialProgressAnimation> with 
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            SizedBox(
-              width: 150,
-              height: 150,
-              child: CircularProgressIndicator(
-                value: widget.progress,
-                strokeWidth: 10,
-                backgroundColor: Colors.grey.shade100,
-                color: widget.color,
-              ),
-            ),
-            Text(
-              '${(widget.progress * 100).toInt()}%',
-              style: const TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ],
+        child: AnimatedBuilder(
+          animation: animation,
+          builder: (context, child) {
+            return Stack(
+              alignment: Alignment.center,
+              children: [
+                SizedBox(
+                  width: 150,
+                  height: 150,
+                  child: CircularProgressIndicator(
+                    value: animation.value,
+                    strokeWidth: 10,
+                    backgroundColor: Colors.grey.shade100,
+                    color: widget.color,
+                  ),
+                ),
+                Text(
+                  '${(animation.value * 100).toInt()}%',
+                  style: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            );
+          }
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          controller.forward();
+        },
         child: const Icon(Icons.start),
       ),
     );
