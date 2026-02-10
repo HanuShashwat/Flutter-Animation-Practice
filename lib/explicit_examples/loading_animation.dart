b4bfff1bbbd2b332e04d7769a9d22d033e3c6e06
Hanu Shashwat <hanushashwat733@gmail.com>
2026-02-10 17:42:48 +0530

    Added a button & basic animation logic.

--- lib/explicit_examples/loading_animation.dart
+++ lib/explicit_examples/loading_animation.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class RadialProgressAnimation extends StatelessWidget {
+class RadialProgressAnimation extends StatefulWidget {
   final double progress;
   final Color color;
 
@@ -9,6 +9,28 @@ class RadialProgressAnimation extends StatelessWidget {
     required this.progress,
     required this.color,
   });
+
+  @override
+  State<RadialProgressAnimation> createState() => _RadialProgressAnimationState();
+}
+
+class _RadialProgressAnimationState extends State<RadialProgressAnimation> with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000)
+    );
+
+    animation = Tween(
+      begin: 0.0,
+      end: widget.progress,
+    ).animate(controller);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,14 +43,14 @@ class RadialProgressAnimation extends StatelessWidget {
               width: 150,
               height: 150,
               child: CircularProgressIndicator(
-                value: progress,
+                value: widget.progress,
                 strokeWidth: 10,
                 backgroundColor: Colors.grey.shade100,
-                color: color,
+                color: widget.color,
               ),
             ),
             Text(
-              '${(progress * 100).toInt()}%',
+              '${(widget.progress * 100).toInt()}%',
               style: const TextStyle(
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
@@ -36,6 +58,10 @@ class RadialProgressAnimation extends StatelessWidget {
             ),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.start),
       ),
     );
   }
