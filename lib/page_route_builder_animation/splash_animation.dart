9f9ba34fd081af42c2011f4ce349ba4c3f3cd44e
Hanu Shashwat <hanushashwat733@gmail.com>
2026-02-11 23:27:39 +0530

    Built the scale animation logic.

--- lib/page_route_builder_animation/splash_animation.dart
+++ lib/page_route_builder_animation/splash_animation.dart
@@ -1,18 +1,66 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class SplashAnimation extends StatelessWidget {
+class SplashAnimation extends StatefulWidget {
   const SplashAnimation({super.key});
+
+  @override
+  State<SplashAnimation> createState() => _SplashAnimationState();
+}
+
+class _SplashAnimationState extends State<SplashAnimation> with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(
+        milliseconds: 500
+      ),
+    );
+    
+    controller.addListener(() {
+      if (controller.isCompleted) {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => const Destination(),
+          ),
+        );
+
+        Timer(const Duration(milliseconds: 500), () {
+          controller.reset();
+        });
+      }
+    });
+
+    scaleAnimation = Tween<double>(
+      begin: 1,
+      end: 11,
+    ).animate(controller);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
-          width: 100,
-          height: 100,
-          decoration: const BoxDecoration(
-            shape: BoxShape.circle,
-            color: Colors.blue,
+        child: GestureDetector(
+          onTap: () {
+            controller.forward();
+          },
+          child: ScaleTransition(
+            scale: scaleAnimation,
+            child: Container(
+              width: 100,
+              height: 100,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.blue,
+              ),
+            ),
           ),
         ),
       ),
